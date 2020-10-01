25fc0949192cd30939409cadea8fb9aa23c8eaa2
finchflinch <40954549+finchflinch@users.noreply.github.com>
2020-10-02 02:43:18 +0530

    added movie properties and image and css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import axios from 'axios';
+import Movie from './Movie';
+
+//css
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
-getMovies = async () => {
-  const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
-}
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies: movies, isLoading: false })
+  }
   componentDidMount() {
     this.getMovies();
+
   }
 
   render() {
     //var declarations
-    const {isLoading} = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : "We are ready."}
-      </div>
-      
-    )
+      <section className="container">
+        {/* ternary operator to check if data is loading */}
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>) :
+          // ternary operator else part
+          (
+            <div className="movies">
+              {movies.map(movie => {
+                return <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres} />
+              })}
+            </div>
+          )}
+      </section>
+    );
   }
 }
 
